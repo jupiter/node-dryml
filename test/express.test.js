e6dc33fdb5f03faff12afe22f39a7520f35ea519
jupiter <pieter@wavana.com>
2011-02-25 17:38:35 +0000

    Ready to publish to npm

--- test/express.test.js
+++ test/express.test.js
@@ -24,18 +24,6 @@ module.exports = {
 		assert.includes(res.body, '<div id="right" class="highlight">');
       });
   },
-  'GET / again': function(){
-    assert.response(app,
-      { url: '/' },
-      { status: 200, headers: { 'Content-Type': 'text/html; charset=utf-8' }},
-      function(res){
-        assert.includes(res.body, '<title>Express</title>');
-		assert.includes(res.body, '<li><a href="/one">One</a></li>');
-		assert.includes(res.body, '<li><a href="/three">Three</a></li>');
-		assert.includes(res.body, '<div class="insert"><a href="#">Printed</a></div>');
-		assert.includes(res.body, '<div id="right" class="highlight">');
-      });
-  },
   'GET /notdefined': function(){
     assert.response(app,
       { url: '/notdefined' },
